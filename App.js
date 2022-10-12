3c135f608cdb4ed99baaa516f2df990cf3a0c101
gabdong <hwan970104@gmail.com>
2022-10-13 02:25:05 +0900

    2022.10.12 - 리액트네이티브 레이아웃설정 강의

--- App.js
+++ App.js
@@ -1,34 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
 
-/**
- * g View       : container -> import 필수
- * g Text       : 모든 text는 Text component 안에 있어야함
- * g StatusBar  : 상단바 -> 시계, wifi, 배터리 등
- */
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
+      <View style={{ flex: 5, backgroundColor: "teal" }}></View>
+      <View style={{ flex: 0.8, backgroundColor: "tomato" }}></View>
     </View>
   );
 }
-
-/**
- * g StyleSheet.create() : 스타일 object 생성, 필수는 아님, component에 style={{style Object}}로 적용 가능, StyleSheet.create() 생략 가능 -> 일반적으로 style Object는 따로 분리함
- * 
- * g web에서 사용하던 style을 모두 사용하지는 못함
- */
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text : {
-    fontSize: '24',
-    color: 'red'
-  }
-});
