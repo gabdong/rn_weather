cc0739090486520d3e9d916151ec3b226d69655d
gabdong <hwan970104@gmail.com>
2022-10-15 02:38:20 +0900

    2022.10.14 React-native dimensions apis, scrollview component

--- App.js
+++ App.js
@@ -1,12 +1,66 @@
 import { StatusBar } from "expo-status-bar";
-import { View } from "react-native";
+import React from "react";
+import { View, StyleSheet, Text, ScrollView, Dimensions } from "react-native";
+
+const { width: SCREEN_WIDTH, SCREEN_HEIGHT } = Dimensions.get("window");
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
-      <View style={{ flex: 5, backgroundColor: "teal" }}></View>
-      <View style={{ flex: 0.8, backgroundColor: "tomato" }}></View>
+    <View style={styles.container}>
+      <StatusBar style="light"></StatusBar>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <ScrollView
+        contentContainerStyle={styles.weather}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    color: "#ffffff",
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+    color: "#ffffff",
+  },
+  description: {
+    marginTop: -20,
+    fontSize: 60,
+    color: "#ffffff",
+  },
+});
